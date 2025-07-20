a9d0047281bfc5d74e651d668c4483605e01a2ad
Waqar Ali <waqar0343930@gmail.com>
2025-07-20 12:49:20 +0500

    adding through JS

---
+++ 03_JS/05_adding_through_JS.js
@@ -0,0 +1,7 @@
+let newBtn = document.createElement("button");
+newBtn.innerText = "Click Me!";
+
+newBtn.style.backgroundColor = "blue";
+newBtn.style.color = "white";
+
+document.querySelector("body").prepend(newBtn);
