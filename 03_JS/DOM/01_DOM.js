50fad223585ed8c83cd136d5a1f42df884e71c89
Waqar Ali <waqar0343930@gmail.com>
2025-08-16 14:34:03 +0500

    4 pillars of DOM

--- 03_JS/DOM/01_DOM.js
+++ 03_JS/DOM/01_DOM.js
@@ -1,3 +1,18 @@
 // 4 pillars of DOM Manipulation
 
 // 1. Selecting elements
+var a = document.querySelector("h1");
+console.log(a);
+
+// 2. chang in HTML
+a.innerHTML = "changed HTML";
+
+// 3. change in CSS
+a.style.color = "red";
+
+// 4. Adding event listeners
+a.addEventListener("click", function () {
+  a.innerHTML = "You clicked me!";
+  a.style.color = "blue";
+  console.log("Element clicked!");
+});
